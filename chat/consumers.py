02da0eacf31981c2e17fc175767e4419e10c5364
Rudreshcg <rudresha.gangaswamaiah@softsuave.com>
2023-06-30 14:07:47 +0530

    updated username to application and updated html code

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,33 +1,36 @@
 import json
 
 from asgiref.sync import async_to_sync
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
+from .models import Messages
 
 
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = "chat_%s" % self.room_name
 
-        async_to_sync(self.channel_layer.group_add)(
-            self.room_group_name, self.channel_name
-        )
-        self.accept()
+        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+        await self.accept()
 
-    def disconnect(self, close_code):
-        async_to_sync(self.channel_layer.group_discard)(
-            self.room_group_name, self.channel_name
-        )
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
-    def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json["message"]
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        message = data["message"]
+        username = data["username"]
+        room = data["room"]
+        save_messages(message, username, room)
 
-        async_to_sync(self.channel_layer.group_send)(
-            self.room_group_name, {"type": "chat_message", "message": message}
-        )
+        await self.channel_layer.group_send(self.room_group_name, {"type": "chat_message", "message": message, "username": username})
 
-    def chat_message(self, event):
+    async def chat_message(self, event):
         message = event["message"]
 
-        self.send(text_data=json.dumps({"message": message}))
+        await self.send(text_data=json.dumps({"message": message}))
+
+
+def save_messages(message, username, room):
+    b = Messages(content=message, username=username, room=room)
+    b.save()
